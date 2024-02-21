eaf32a2623ebaf1a2a5a2b5f22ad435cb063a358
Ayoub Acharki <A.Acharki-1@student.tudelft.nl>
2024-02-21 18:45:01 +0100

    adding the expense code and test code2

---
+++ commons/src/test/java/commons/ExpenseTest.java
@@ -0,0 +1,76 @@
+package commons;
+
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenseTest {
+
+    private static final Event SOME_EVENT = new Event("Drinkfest");
+    private static final Participant SOME_PARTICIPANT = new Participant("A", "B");
+
+    @Test
+    public void checkConstructor() {
+        var e = new Expense(SOME_EVENT, SOME_PARTICIPANT, 4.50, new Date(2024-11-10), "snack", "food");
+        assertEquals(SOME_EVENT, e.event);
+        assertEquals(SOME_PARTICIPANT, e.participant);
+        assertNotNull(e.expense);
+    }
+
+    @Test
+    void getId() {
+
+    }
+
+    @Test
+    void getEventId() {
+    }
+
+    @Test
+    void getCreditorId() {
+    }
+
+    @Test
+    void getAmount() {
+    }
+
+    @Test
+    void getDate() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getTag() {
+    }
+
+    @Test
+    void setAmount() {
+    }
+
+    @Test
+    void setDate() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void setTag() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
