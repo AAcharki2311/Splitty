de2582a96e5462d10d5eb2a012deddc7dc7a3937
Thijmen Verkerk <t.j.verkerk-1@student.tudelft.nl>
2024-02-21 15:56:30 +0100

    Added OOP Checkstyle as a base and the basic variables and getters/setters, equals and hash for Events class

---
+++ commons/src/main/java/commons/Event.java
@@ -0,0 +1,96 @@
+package commons;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private long code;
+    private String name;
+    private Date creationDate;
+    private Date lastActDate;
+
+    /**
+     * The getter for the Event-ID
+     * @return the Event-ID
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * The getter for the access code of the Event
+     * @return the access code of the Event
+     */
+    public long getCode() {
+        return code;
+    }
+
+    /**
+     * The getter for the name of the Event
+     * @return the name of the Event
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * The getter for the creation date of the Event
+     * @return the creation date of the Event
+     */
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    /**
+     * The getter for the date of the last activity of the Event
+     * @return the last activity date of the Event
+     */
+    public Date getLastActDate() {
+        return lastActDate;
+    }
+
+    /**
+     * The setter for the name of the Event
+     * @param name the name to set the name of the Event to
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * The setter for the date of the last activity of the Event
+     * @param lastActDate the date to set the last activity date of the Event to
+     */
+    public void setLastActDate(Date lastActDate) {
+        this.lastActDate = lastActDate;
+    }
+
+    /**
+     * The equality method for Event
+     * Events are considered equal iff they are both Events and their id's match
+     * @param o the object to compare to
+     * @return True if equal
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
