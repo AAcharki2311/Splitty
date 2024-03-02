ac8a5b9bc8d18a74ff1d8901fc8fe13ce8db605e
Giulia Meneghello <g.meneghello@student.tudelft.nl>
2024-03-02 16:28:24 +0100

    Add the controller in api

---
+++ server/src/main/java/server/api/EventController.java
@@ -0,0 +1,17 @@
+package server.api;
+
+import org.springframework.web.bind.annotation.*;
+
+import commons.Event;
+import server.database.EventRepository;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+    // @PostMapping
+    // public Event addEvent(@RequestBody Event event) {
+        // Add method
+    // }
+
+    // @GetMapping
+}
