2522d855a10ed31c48030836c8cac3145a46ed8a
Giulia Meneghello <g.meneghello@student.tudelft.nl>
2024-03-03 20:26:09 +0100

    Add the controller for Payment

---
+++ server/src/main/java/server/api/PaymentController.java
@@ -0,0 +1,44 @@
+package server.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import commons.Payment;
+import server.database.PaymentRepository;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentController {
+    private final Random random;
+    private final PaymentRepository paymentRepository;
+
+    /**
+     * Constructor for the controller of the payments
+     *
+     * @param random variable random
+     * @param paymentRepository repository for payments
+     */
+    public PaymentController(Random random, PaymentRepository paymentRepository) {
+        this.random = random;
+        this.paymentRepository = paymentRepository;
+    }
+
+    /**
+     * Method to create a payment and add it to the repository
+     *
+     * @param payment the payment that will be added
+     * @return the response from the action
+     */
+    @PostMapping(path = {"", "/"})
+    public ResponseEntity<Payment> add(@RequestBody Payment payment) {
+        if (payment == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        Payment postedPayment = paymentRepository.save(payment);
+        return ResponseEntity.ok(postedPayment);
+    }
+
+    // method to select sorted
+
+}
