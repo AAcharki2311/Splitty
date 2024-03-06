1b19623216aa752de768654ac38629adef083d5f
Anne-Sophie Straathof <ahcstraathof@student.tudelft.nl>
2024-03-06 14:33:29 +0100

    Added some javadoc comments, to pass checkstyle

--- client/src/main/java/client/scenes/AdminDashboardCtrl.java
+++ client/src/main/java/client/scenes/AdminDashboardCtrl.java
@@ -8,11 +8,19 @@ public class AdminDashboardCtrl {
 
     private final MainCtrl mc;
 
+    /**
+     * Constructor for the AdminDashboardCtrl
+     * @param mc the main controller
+     */
     @Inject
     public AdminDashboardCtrl(MainCtrl mc) {
         this.mc = mc;
     }
 
+    /**
+     * Used to get back to the Start Screen
+     * @throws IOException if something went wrong with showing the start screen
+     */
     public void clickStart() throws IOException {
         mc.showStart();
     }
