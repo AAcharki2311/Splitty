a758511f1cfe22ba7ba82b5698cbad3511859a89
Ayoub Acharki <A.Acharki-1@student.tudelft.nl>
2024-03-06 14:31:22 +0100

    Adding javadoc

--- client/src/main/java/client/scenes/EditParticipantCtrl.java
+++ client/src/main/java/client/scenes/EditParticipantCtrl.java
@@ -29,17 +29,28 @@ public class EditParticipantCtrl implements Initializable, languageSwitchInterfa
     //    here must go an array with names of participants
     private String[] names = {"John", "Chris", "Anna"};
 
+    /**
+     * Constructor of the EditParticipantCtrl
+     * @param mc represent the MainCtrl
+     */
     @Inject
     public EditParticipantCtrl(MainCtrl mc) {
         this.mc = mc;
     }
 
+    /**
+     * This methods sets the image for the imageview and adds the items to the comboboxes
+     * @param url represent the URL
+     * @param resourceBundle represent the ResourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         comboBoxParticipants.getItems().addAll(names);
     }
 
-
+    /**
+     * This method checks if the input is correct
+     */
     public void check() {
 //        TextFieldMoney.textProperty().addListener((observable, oldValue, newValue) -> {
 //            if (!newValue.matches("\\d*\\.?\\d*")) { // Regex to allow digits and optional decimal point
@@ -48,10 +59,16 @@ public class EditParticipantCtrl implements Initializable, languageSwitchInterfa
 //        });
     }
 
+    /**
+     * Method of the cancel button, when pressed, it shows the eventoverview screen
+     */
     public void clickBack() {
         mc.showEventOverview();
     }
 
+    /**
+     * Method of the save button, it gets all user input and when pressed, it shows the eventoverview screen
+     */
     public void getAllInfo() {
         String name = TextFieldName.getText();
         String email = TextFieldEmail.getText();
@@ -61,6 +78,10 @@ public class EditParticipantCtrl implements Initializable, languageSwitchInterfa
         System.out.println(meta.toString());
     }
 
+    /**
+     * This method translates each label. It changes the text to the corresponding key with the translated text
+     * @param taal the language that the user wants to switch to
+     */
     @Override
     public void langueageswitch(String taal) {
 
