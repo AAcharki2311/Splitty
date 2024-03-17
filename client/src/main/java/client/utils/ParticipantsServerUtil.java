5bcec5049fee0bda7c5216a3a4400e7c9e0d4e72
eelco de jong <e.k.dejong@student.tudelft.nl>
2024-03-17 22:52:46 +0100

    added ParticipantsServerUtil class, with methods to get all or a specific participant and the ability to delete or update a participant

---
+++ client/src/main/java/client/utils/ParticipantsServerUtil.java
@@ -0,0 +1,52 @@
+package client.utils;
+
+import com.sun.javafx.application.ParametersImpl;
+import commons.Participant;
+import jakarta.ws.rs.client.ClientBuilder;
+
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.Response;
+import org.glassfish.jersey.client.ClientConfig;
+
+import java.util.List;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+
+public class ParticipantsServerUtil {
+    private static final String SERVER = "https://localhost:8080/api/participants";
+
+    public List<Participant> getAllParticipants() {
+        return ClientBuilder.newClient(new ClientConfig())
+                .target(SERVER)
+                .request(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
+                .get(new GenericType<List<Participant>>() {});
+    }
+
+    public Participant getParticipantByID(long id) {
+        return ClientBuilder.newClient(new ClientConfig())
+                .target(SERVER).path("/"+id)
+                .request(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
+                .get(new GenericType<Participant>() {});
+    }
+
+    public Participant updateParticipantByID(long id, Participant participant) {
+        return ClientBuilder.newClient(new ClientConfig())
+                .target(SERVER).path("/"+id)
+                .request(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
+                .put(Entity.entity(participant,APPLICATION_JSON), Participant.class);
+    }
+
+    public boolean deleteParticipantByID(long id) {
+        Response response = ClientBuilder.newClient(new ClientConfig())
+                .target(SERVER).path("/"+id)
+                .request(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
+                .delete();
+        return response.getStatus() == 204;
+
+    }
+}
