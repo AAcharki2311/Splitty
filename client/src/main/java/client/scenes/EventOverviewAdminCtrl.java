72a6ae8cafdb9efe40601ee023b334ac5be03340
Anne-Sophie Straathof <ahcstraathof@student.tudelft.nl>
2024-03-23 09:27:03 +0100

    Updated the visuals of EventOverViewAdminCTRL. Tables are now being shown which can be filled with the Expenses and Participants of the Event

--- client/src/main/java/client/scenes/EventOverviewAdminCtrl.java
+++ client/src/main/java/client/scenes/EventOverviewAdminCtrl.java
@@ -3,24 +3,63 @@ package client.scenes;
 import client.utils.EventServerUtils;
 import com.google.inject.Inject;
 import commons.Event;
+//import javafx.beans.property.SimpleStringProperty;
+import commons.Expense;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 // import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 // import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.net.URL;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.ResourceBundle;
 
 
-public class EventOverviewAdminCtrl {
-    private final MainCtrl mc;
+public class EventOverviewAdminCtrl implements Initializable {
+
+    /** BASIS **/
     private final EventServerUtils server;
-
-    private long eventid;
-
+    private final MainCtrl mc;
+    /** MENU **/
     @FXML
-    private Label eventname;
+    private ImageView imgSet;
+    @FXML
+    private ImageView imgArrow;
+    @FXML
+    private ImageView imgHome;
+    @FXML
+    private ImageView imageviewFlag;
+    /** PAGE **/
+    private long eventid;
+    @FXML
+    private Text eventname;
+    /** TABLE EXPENSES **/
+    @FXML
+    private TableView<Expense> tableExp;
+    @FXML
+    private TableColumn<Expense, String> colDate;
+    @FXML
+    private TableColumn<Expense, String> colAm;
+    @FXML
+    private TableColumn<Expense, String> colPart;
+    @FXML
+    private TableColumn<Expense, String> colTitle;
+    @FXML
+    private TableColumn<Expense, String> colTag;
+    /** TABLE PARTICIPANTS **/
+    @FXML
+    private TableView<Expense> tablePart;
+    @FXML
+    private TableColumn<Expense, String> colName;
+    @FXML
+    private TableColumn<Expense, String> colEmail;
 
     /**
      * Constructer for the AdminEvent Controller
@@ -32,6 +71,31 @@ public class EventOverviewAdminCtrl {
         this.mc = m;
         this.server = server;
         this.eventid = 0;
+    }
+
+    /**
+     *
+     * @param location
+     * The location used to resolve relative paths for the root object, or
+     * {@code null} if the location is not known.
+     *
+     * @param resources
+     * The resources used to localize the root object, or {@code null} if
+     * the root object was not localized.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        Format formatter = new SimpleDateFormat("dd-MM-yyyy");
+//        colDate.setCellValueFactory(q -> new SimpleStringProperty(String.valueOf(q.getValue().)));
+//        colId.setCellValueFactory(q -> new SimpleStringProperty(String.valueOf(q.getValue().id)));
+//        colName.setCellValueFactory(q -> new SimpleStringProperty(q.getValue().name));
+//        colDate1.setCellValueFactory(q -> new SimpleStringProperty(formatter.format(q.getValue().creationDate)));
+//        colDate2.setCellValueFactory(q -> new SimpleStringProperty(formatter.format(q.getValue().lastActDate)));
+//        refresh();
+        imgSet.setImage(new Image("images/settings.png"));
+        imgArrow.setImage(new Image("images/arrow.png"));
+        imgHome.setImage(new Image("images/home.png"));
+        imageviewFlag.setImage(new Image("images/br-circle-01.png"));
     }
 
     /**
@@ -69,5 +133,30 @@ public class EventOverviewAdminCtrl {
         Event x = server.getEventByID(eid);
         // get the information from the database
         eventname.setText(x.getName());
+    }
+
+    /**
+     * Used to get back to the Start Screen
+     * @throws IOException if something went wrong with showing the start screen
+     */
+    public void clickHome() throws IOException {
+        mc.showStart();
+    }
+
+    /**
+     * Used to get back to the Screen before this
+     * @throws IOException if something went wrong with showing the start screen
+     */
+    public void clickBack() throws IOException {
+        mc.showAdminDashboard();
+    }
+
+    /**
+     * javadoc
+     */
+    public void refresh(){
+//        var expenses = server.getAllEvents();
+//        data = FXCollections.observableList(events);
+//        table.setItems(data);
     }
 }
