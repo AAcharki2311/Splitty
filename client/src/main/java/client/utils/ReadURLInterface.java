91ed8cc8e08d6acbfd6d850b3a0f1362823fe40e
Ayoub Acharki <A.Acharki-1@student.tudelft.nl>
2024-03-27 15:53:24 +0100

    checkstyle fix

--- client/src/main/java/client/utils/ReadURLInterface.java
+++ client/src/main/java/client/utils/ReadURLInterface.java
@@ -1,6 +1,15 @@
 package client.utils;
 
 public interface ReadURLInterface {
+    /**
+     * Read the server URL from the file
+     * @return the server URL
+     */
     String readServerUrl();
+
+    /**
+     * Write the server URL to the file
+     * @param url the server URL
+     */
     void writeServerUrl(String url);
 }
