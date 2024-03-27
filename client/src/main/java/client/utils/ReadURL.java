ad12e884d23d2c121fdcf2ca6288a4abefd4ff30
Ayoub Acharki <A.Acharki-1@student.tudelft.nl>
2024-03-27 14:40:52 +0100

    Fixed the readurl class

--- client/src/main/java/client/utils/ReadURL.java
+++ client/src/main/java/client/utils/ReadURL.java
@@ -1,6 +1,7 @@
 package client.utils;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -9,15 +10,41 @@ public class ReadURL {
      * This method reads the config file and returns the server url
      * @return server url
      */
-    public String readServerUrl() throws IOException {
-        Properties appProps = new Properties();
-        String configFilePath = "src/main/resources/configfile.properties";
+    public String readServerUrl() {
+        try{
+            Properties appProps = new Properties();
+            String configFilePath = "src/main/resources/configfile.properties";
 
-        FileInputStream inputStream = new FileInputStream(configFilePath);
-        appProps.load(inputStream);
-        inputStream.close();
+            FileInputStream inputStream = new FileInputStream(configFilePath);
+            appProps.load(inputStream);
+            inputStream.close();
 
-        String url = appProps.getProperty("serverurl");
-        return url;
+            String url = appProps.getProperty("serverurl");
+            return url;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * This method reads the config file and returns the server url
+     * @param url the url that the user wants to switch to
+     */
+    public void writeServerUrl(String url) {
+        try {
+            Properties appProps = new Properties();
+            String configFilePath = "src/main/resources/configfile.properties";
+            FileInputStream inputStream = new FileInputStream(configFilePath);
+            appProps.load(inputStream);
+            inputStream.close();
+
+            appProps.setProperty("serverurl", url);
+
+            FileOutputStream outputStream = new FileOutputStream(configFilePath);
+            appProps.store(outputStream, null);
+            outputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
