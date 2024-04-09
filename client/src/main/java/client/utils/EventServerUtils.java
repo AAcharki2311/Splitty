9f67b68d21e4fffd85aefee5e2989415b9d2be0c
Ayoub Acharki <A.Acharki-1@student.tudelft.nl>
2024-04-09 18:40:28 +0200

    pipeline fix with checkstyle error

--- client/src/main/java/client/utils/EventServerUtils.java
+++ client/src/main/java/client/utils/EventServerUtils.java
@@ -10,7 +10,6 @@ import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.Response;
 import org.glassfish.jersey.client.ClientConfig;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
