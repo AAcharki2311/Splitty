7ac20373efbb9794a271a1ddd7729af4de55eedb
Ayoub Acharki <A.Acharki-1@student.tudelft.nl>
2024-04-10 13:51:25 +0200

    Fixing checkstyle2

--- client/src/test/java/client/utils/WriteEventNamesTest.java
+++ client/src/test/java/client/utils/WriteEventNamesTest.java
@@ -2,8 +2,6 @@ package client.utils;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
