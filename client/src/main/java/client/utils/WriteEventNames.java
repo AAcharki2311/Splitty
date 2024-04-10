d0700ce4b9da5737fdb943d490305fb39f78641d
Ayoub Acharki <A.Acharki-1@student.tudelft.nl>
2024-04-10 13:49:56 +0200

    Fixing checkstyle

--- client/src/main/java/client/utils/WriteEventNames.java
+++ client/src/main/java/client/utils/WriteEventNames.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
