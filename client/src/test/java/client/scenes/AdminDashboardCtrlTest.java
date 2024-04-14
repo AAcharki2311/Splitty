2fb06529a17a0b8465648d1b7365ee6c80d2cee9
Anne-Sophie Straathof <ahcstraathof@student.tudelft.nl>
2024-04-14 16:39:08 +0200

    Checkstyle

--- client/src/test/java/client/scenes/AdminDashboardCtrlTest.java
+++ client/src/test/java/client/scenes/AdminDashboardCtrlTest.java
@@ -1,7 +1,7 @@
 package client.scenes;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
 
 class AdminDashboardCtrlTest {
 
