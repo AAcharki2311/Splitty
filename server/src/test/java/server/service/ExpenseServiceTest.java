734a5e2f815a8261899e4256bbf55ea4d1a980a2
Giulia Meneghello <g.meneghello@student.tudelft.nl>
2024-04-14 13:58:05 +0200

    Finish tests for ExpenseService

--- server/src/test/java/server/service/ExpenseServiceTest.java
+++ server/src/test/java/server/service/ExpenseServiceTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import server.repository.TestExpenseRepository;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ExpenseServiceTest {
     private TestExpenseRepository expenseRepository;
@@ -45,4 +46,95 @@ public class ExpenseServiceTest {
         assertEquals(expense1, list.get(0));
         assertEquals(expense2, list.get(1));
     }
+
+    /**
+     * Test for the method getById
+     */
+    @Test
+    public void getByIdTest() {
+        expenseService.add(expense1);
+        long id1 = expense1.getId();
+        expenseService.add(expense2);
+        long id2 = expense2.getId();
+        assertTrue(expenseService.getById(id1).isPresent());
+        assertEquals(expense1, expenseService.getById(id1).get());
+        assertTrue(expenseService.getById(id2).isPresent());
+        assertNotEquals(expense2, expenseService.getById(id2).get());
+    }
+
+    /**
+     * Test for the method getById if it's non-existing
+     */
+    @Test
+    public void getByWrongIdTest() {
+        long id = -1;
+        assertFalse(expenseService.getById(id).isPresent());
+    }
+
+    /**
+     * Test for the update method
+     */
+    @Test
+    public void updateTest() {
+        expenseService.add(expense1);
+        Expense updatedExpense = new Expense(eventTest, participantTest1, 4.5, dateTest2, "titleUpdate","tagUpdate", "curUpdate");
+        assertTrue(expenseService.update(expense1.getId(), updatedExpense).isPresent());
+        assertEquals(updatedExpense.getEvent(), expenseService.update(expense1.getId(), updatedExpense).get().getEvent()); // Check the updated event of the Expense
+        assertEquals(updatedExpense.getAmount(), expenseService.update(expense1.getId(), updatedExpense).get().getAmount()); // Check the updated amount of the Expense
+        assertEquals(updatedExpense.getDate(), expenseService.update(expense1.getId(), updatedExpense).get().getDate()); // Check the updated date of the Expense
+        assertEquals(updatedExpense.getTitle(), expenseService.update(expense1.getId(), updatedExpense).get().getTitle()); // Check the updated title of the Expense
+        assertEquals(updatedExpense.getTag(), expenseService.update(expense1.getId(), updatedExpense).get().getTag()); // Check the updated tag of the Expense
+    }
+
+    /**
+     * Test for the delete method
+     *
+    @Test
+    public void deleteTest() {
+        expenseService.add(expense1);
+        ResponseEntity<Void> responseEntity = expenseService.delete(expense1.getId());
+        assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode()); // Check status code
+        assertNull(responseEntity.getBody()); // Check that the object is null
+    }
+    */
+
+    /**
+     * Test for the method getSortedExpensesTitle
+     */
+    @Test
+    public void getSortedTitleTest() {
+        expenseService.add(expense1);
+        expenseService.add(expense2);
+        List<Expense> checkExpense = new ArrayList<>();
+        checkExpense.add(expense2);
+        checkExpense.add(expense1);
+        assertEquals(checkExpense, expenseService.getSortedExpensesTitle()); // Check return list
+    }
+
+    /**
+     * Test for the method getSortedExpensesPerson
+     */
+    @Test
+    public void getSortedPersonTest() {
+        expenseService.add(expense1);
+        expenseService.add(expense2);
+        List<Expense> checkExpense = new ArrayList<>();
+        checkExpense.add(expense2);
+        checkExpense.add(expense1);
+        assertEquals(checkExpense, expenseService.getSortedExpensesPerson()); // Check return list
+    }
+
+    /**
+     * Test for the method getSortedExpensesDate
+     */
+    @Test
+    public void getSortedDate() {
+        expenseService.add(expense1);
+        expenseService.add(expense2);
+        List<Expense> checkExpense = new ArrayList<>();
+        checkExpense.add(expense1);
+        checkExpense.add(expense2);
+        assertEquals(checkExpense, expenseService.getSortedExpensesDate()); // Check return list
+    }
+
 }
