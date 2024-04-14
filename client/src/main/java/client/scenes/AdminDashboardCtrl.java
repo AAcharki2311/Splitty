513e2c99a13fd6b3fe4f661899d29273dbf681f4
Ayoub Acharki <a.acharki-1@student.tudelft.nl>
2024-04-14 16:03:53 +0200

    Update AdminDashboardCtrl.java

--- client/src/main/java/client/scenes/AdminDashboardCtrl.java
+++ client/src/main/java/client/scenes/AdminDashboardCtrl.java
@@ -173,25 +173,25 @@ public class AdminDashboardCtrl implements Initializable {
         }
     }
 
-    /**
-     * Shows error message 2
-     */
-    public void showError2() {
-        try {
-            Event test = server.getEventByID(Long.parseLong(eid));
-        }
-        catch (Exception e){
-            JOptionPane.showOptionDialog(null, h.get("key117"),h.get("key114"), JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, new Object[]{}, null);
-//            imgMessage.setImage(new Image("images/notifications/Slide1.png"));
-//            PauseTransition pause = new PauseTransition(Duration.seconds(6));
-//            pause.setOnFinished(p -> imgMessage.setImage(null));
-//            pause.play();
-            return;
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+//     /**
+//      * Shows error message 2
+//      */
+//     public void showError2() {
+//         try {
+//             Event test = server.getEventByID(Long.parseLong(eid));
+//         }
+//         catch (Exception e){
+//             JOptionPane.showOptionDialog(null, h.get("key117"),h.get("key114"), JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, new Object[]{}, null);
+// //            imgMessage.setImage(new Image("images/notifications/Slide1.png"));
+// //            PauseTransition pause = new PauseTransition(Duration.seconds(6));
+// //            pause.setOnFinished(p -> imgMessage.setImage(null));
+// //            pause.play();
+//             return;
+//         }
+//         catch (Exception e){
+//             e.printStackTrace();
+//         }
+//     }
 
     /**
      * Check for valid input
